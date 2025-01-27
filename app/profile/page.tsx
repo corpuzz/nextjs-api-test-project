4538ce93f636b481a004cb3ec812d84bf53869f3
corpuzz <corpuz.markjanlee@gmail.com>
2025-01-27 15:46:31 +0800

    fixed spelling

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,18 +3,18 @@
 import { useState } from "react"
 
 export default function Page() {
-  const [qoute, setQoute] = useState()
+  const [quote, setQuote] = useState()
 
   const handleFetch = async () => {
     // const result = await fetch('/api/getData')
     const result = await fetch('https://nxttt.vercel.app/api/generateQoute')
     const data = await result.json()
-    setQoute(data.qoute)
+    setQuote(data.quote)
   }
 
   return (
     <div className="flex flex-col w-full h-screen items-center justify-center" id="div">
-      <h1 className="text-4xl">{qoute}</h1>
+      <h1 className="text-4xl">{quote}</h1>
 
       {/* <button >Fetch Participants</button> */}
         <button onClick={handleFetch} className="bg-gray-200 p-5 rounded-full hover:bg-blue-500 transition text-black">Fetch Qoute</button>

8d95f966531a9f7346fcd28f20d36a0462343b03
corpuzz <corpuz.markjanlee@gmail.com>
2025-01-27 15:30:16 +0800

    fixed fetch endpoint

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,34 +1,24 @@
-// export default function Page() {
-//     return (
-//         <>
-//         <div className="flex flex-col w-full h-screen items-center justify-center" id="div">
-//             <h1 className="text-4xl">Profile Page</h1>
-//         </div>
-//         </>
-//     )
-// }
-
 
 "use client"
 
 import { useState } from "react"
 
-
 export default function Page() {
-  const [participants, setParticipants] = useState()
+  const [qoute, setQoute] = useState()
 
   const handleFetch = async () => {
-    const result = await fetch('/api/getData')
+    // const result = await fetch('/api/getData')
+    const result = await fetch('https://nxttt.vercel.app/api/generateQoute')
     const data = await result.json()
-    setParticipants(data.data)
+    setQoute(data.data)
   }
 
   return (
     <div className="flex flex-col w-full h-screen items-center justify-center" id="div">
-      <h1 className="text-4xl">{participants}</h1>
+      <h1 className="text-4xl">{qoute}</h1>
 
       {/* <button >Fetch Participants</button> */}
-        <button onClick={handleFetch} className="bg-gray-200 p-5 rounded-full hover:bg-blue-500 transition text-black">Fetch Participants</button>
+        <button onClick={handleFetch} className="bg-gray-200 p-5 rounded-full hover:bg-blue-500 transition text-black">Fetch Qoute</button>
     </div>
   )
 }
